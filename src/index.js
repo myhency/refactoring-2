4e3470a9d83bdbac535e938076f02ce077867cf4
myhency <hency.yeo@gmail.com>
2023-09-18 19:13:21 +0900

    refactor: format 변수 제거하기

--- src/index.js
+++ src/index.js
@@ -41,15 +41,18 @@ function volumeCreditsFor(aPerformance) {
     return result;
 }
 
+function format(aNumber) {
+    return new Intl.NumberFormat("en-US", {
+        style: "currency",
+        currency: "USD",
+        minimumFractionDigits: 2,
+    }).format(aNumber);
+}
+
 function statement(invoice, plays) {
     let totalAmount = 0;
     let volumeCredits = 0;
     let result = `청구 내역 (고객명: ${invoice.customer})\n`;
-    const format = new Intl.NumberFormat("en-US", {
-        style: "currency",
-        currency: "USD",
-        minimumFractionDigits: 2,
-    }).format;
 
     for (let perf of invoice.performances) {
         volumeCredits += volumeCreditsFor(perf);
