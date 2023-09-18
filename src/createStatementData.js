1ad4d56b879b056ced0d29809b7be2bb550a3c6a
myhency <hency.yeo@gmail.com>
2023-09-18 22:53:02 +0900

    refactor: 타입 코드를 서브클래스로 바꾸기

--- src/createStatementData.js
+++ src/createStatementData.js
@@ -1,7 +1,13 @@
 function createPerformanceCalculator(aPerformance, aPlay) {
-    return new PerformanceCalculator(aPerformance, aPlay);
+    switch (aPlay.type) {
+        case "tragedy":
+            return new TragedyCalculator(aPerformance, aPlay);
+        case "comedy":
+            return new ComedyCalculator(aPerformance, aPlay);
+        default:
+            throw new Error(`알 수 없는 장르: ${aPlay.type}`);
 }
-
+}
 class PerformanceCalculator {
     constructor(aPerformance, aPlay) {
         this.performance = aPerformance;
