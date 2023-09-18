d65746bee4adf0be972a423a54ac74be86edb9b2
myhency <hency.yeo@gmail.com>
2023-09-18 22:37:10 +0900

    refactor: amountFor 함수를 계산기로 옮기기 완료

--- src/createStatementData.js
+++ src/createStatementData.js
@@ -44,7 +44,7 @@ export default function createStatementData(invoice, plays) {
         );
         const result = Object.assign({}, aPerformance);
         result.play = calculator.play;
-        result.amount = amountFor(result);
+        result.amount = calculator.amount;
         result.volumeCredits = volumeCreditsFor(result);
         return result;
     }
