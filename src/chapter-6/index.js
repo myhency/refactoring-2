5003a389ffce390884e2eeda2e99cf8fb6906ea0
myhency <hency.yeo@gmail.com>
2023-09-19 14:58:00 +0900

    refactor: 지역 변수를 사용할 때의 리팩터링 추가 완료

--- src/chapter-6/index.js
+++ src/chapter-6/index.js
@@ -35,27 +35,30 @@ function printOwing(invoice) {
         outStanding += o.amount;
     }
 
-    // 마감일(dueDate)을 기록한다.
-    const today = Clock.today;
-    invoice.dueDate = new Date(
-        today.getFullYear(),
-        today.getMonth(),
-        today.getDate() + 30
-    );
+    recordDueDate(invoice);
 
-    printDetails();
+    printDetails(invoice, outStanding);
 
     function printBanner() {
         console.log("***********************");
         console.log("**** 고객 채무 ****");
         console.log("***********************");
     }
+}
 
-    function printDetails() {
-        console.log(`고객명: ${invoice.customer}`);
-        console.log(`채무액: ${outStanding}`);
-        console.log(`마감일: ${invoice.dueDate.toLocaleDateString()}`);
-    }
+function recordDueDate(invoice) {
+    const today = Clock.today;
+    invoice.dueDate = new Date(
+        today.getFullYear(),
+        today.getMonth(),
+        today.getDate() + 30
+    );
+}
+
+function printDetails(invoice, outStanding) {
+    console.log(`고객명: ${invoice.customer}`);
+    console.log(`채무액: ${outStanding}`);
+    console.log(`마감일: ${invoice.dueDate.toLocaleDateString()}`);
 }
 
 export { printOwing };
