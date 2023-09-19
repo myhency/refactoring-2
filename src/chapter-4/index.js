04df9be99dc6198a7492121f37ddd403d60b8f8c
myhency <hency.yeo@gmail.com>
2023-09-19 13:24:34 +0900

    fix: 잘못작성된 부분 수정

--- src/chapter-4/index.js
+++ src/chapter-4/index.js
@@ -53,6 +53,27 @@ class Province {
     get shortfall() {
         return this._demand - this.totalProduction;
     }
+    get profit() {
+        return this.demandValue - this.demandCost;
+    }
+    get demandValue() {
+        return this.satisfiedDemand * this.price;
+    }
+    get satisfiedDemand() {
+        return Math.min(this._demand, this.totalProduction);
+    }
+    get demandCost() {
+        let remainingDemand = this.demand;
+        let result = 0;
+        this.producers
+            .sort((a, b) => a.cost - b.cost)
+            .forEach((p) => {
+                const contribution = Math.min(remainingDemand, p.production);
+                remainingDemand -= contribution;
+                result += contribution * p.cost;
+            });
+        return result;
+    }
 }
 
 class Producer {
@@ -79,27 +100,6 @@ class Producer {
         const newProduction = Number.isNaN(amount) ? 0 : amount;
         this._province.totalProduction += newProduction - this._production;
         this._production = newProduction;
-    }
-    get profit() {
-        return this.demandValue - this.demandCost;
-    }
-    get demandValue() {
-        return this.satisfiedDemand * this.price;
-    }
-    get satisfiedDemand() {
-        return Math.min(this._demand, this.totalProduction);
-    }
-    get demandCost() {
-        let remainingDemand = this.demand;
-        let result = 0;
-        this.producers
-            .sort((a, b) => a.cost - b.cost)
-            .forEach((p) => {
-                const contribution = Math.min(remainingDemand, p.production);
-                remainingDemand -= contribution;
-                result += contribution * p.cost;
-            });
-        return result;
     }
 }
 
