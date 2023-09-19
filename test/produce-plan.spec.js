00395e7f0fb661ce0421fd88fe86ff718f2f44d8
myhency <hency.yeo@gmail.com>
2023-09-19 13:24:48 +0900

    feat: 테스트 추가

--- test/produce-plan.spec.js
+++ test/produce-plan.spec.js
@@ -12,4 +12,8 @@ describe("province", () => {
         const asia = new Province(sampleProvinceData());
         expect(asia.shortfall).equal(5);
     });
+    it("profit", () => {
+        const asia = new Province(sampleProvinceData());
+        expect(asia.profit).equal(230);
+    });
 });
