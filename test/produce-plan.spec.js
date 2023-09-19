8b639247e0bdfd29ef48ffda9f3b18340102d9e8
myhency <hency.yeo@gmail.com>
2023-09-19 13:37:47 +0900

    feat: 경계 조건 검사하기 - sad path 추가

--- test/produce-plan.spec.js
+++ test/produce-plan.spec.js
@@ -22,4 +22,53 @@ describe("province", () => {
         expect(asia.shortfall).equal(-6);
         expect(asia.profit).equal(292);
     });
+    describe("no producers", () => {
+        // 생산자가 없는 경우
+        let noProducers;
+        beforeEach(() => {
+            const data = {
+                name: "No producers",
+                producers: [],
+                demand: 30,
+                price: 20,
+            };
+            noProducers = new Province(data);
+        });
+        it("shortfall", () => {
+            expect(noProducers.shortfall).equal(30);
+        });
+        it("profit", () => {
+            expect(noProducers.profit).equal(0);
+        });
+        it("zero demand", () => {
+            // 수요가 없을때
+            asia.demand = 0;
+            expect(asia.shortfall).equal(-25);
+            expect(asia.profit).equal(0);
+        });
+        it("negative demand", () => {
+            // 수요가 마이너스일때
+            asia.demand = -1;
+            expect(asia.shortfall).equal(-26);
+            expect(asia.profit).equal(-10);
+        });
+        it("empty string demand", () => {
+            // 수요가 빈 문자열일때
+            asia.demand = "";
+            expect(asia.shortfall).NaN;
+            expect(asia.profit).NaN;
+        });
+    });
+    describe("string for producers", () => {
+        it("", () => {
+            const data = {
+                name: "String producers",
+                producers: "",
+                demand: 30,
+                price: 20,
+            };
+            const prov = new Province(data);
+            expect(prov.shortfall).equal(0);
+        });
+    });
 });
