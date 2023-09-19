64317c5ae91cd0ab6d376dfe95552a36a2a36642
myhency <hency.yeo@gmail.com>
2023-09-19 14:52:32 +0900

    feat: 유효범위를 벗어나는 변수가 없을때의 기본 코드 추가

--- src/chapter-6/index.js
+++ src/chapter-6/index.js
@@ -1,22 +1,53 @@
-import { invoices } from "../data/index.js";
+import { owingInvoice as invoices } from "../data/index.js";
 
-function printBanner() {
-    console.log("***********************");
-}
+// function printBanner() {
+//     console.log("***********************");
+// }
 
-function calculateOutstanding() {
-    return 10;
+// function calculateOutstanding() {
+//     return 10;
+// }
+
+// function printOwing(invoice) {
+//     printBanner();
+//     let outStanding = calculateOutstanding();
+//     printDetails();
+
+//     function printDetails() {
+//         console.log(`고객명: ${invoice.customer}`);
+//         console.log(`채무액: ${outStanding}`);
+//     }
+// }
+
+class Clock {
+    static get today() {
+        return new Date(); // returns the current date
+    }
 }
 
 function printOwing(invoice) {
-    printBanner();
-    let outStanding = calculateOutstanding();
-    printDetails();
+    let outStanding = 0;
+    console.log("***********************");
+    console.log("**** 고객 채무 ****");
+    console.log("***********************");
 
-    function printDetails() {
-        console.log(`고객명: ${invoice.customer}`);
-        console.log(`채무액: ${outStanding}`);
+    // 미해결 채무(outstanding)를 계산한다.
+    for (const o of invoice.orders) {
+        outStanding += o.amount;
     }
+
+    // 마감일(dueDate)을 기록한다.
+    const today = Clock.today;
+    invoice.dueDate = new Date(
+        today.getFullYear(),
+        today.getMonth(),
+        today.getDate() + 30
+    );
+
+    // 세부 사항을 출력한다.
+    console.log(`고객명: ${invoice.customer}`);
+    console.log(`채무액: ${outStanding}`);
+    console.log(`마감일: ${invoice.dueDate.toLocaleDateString()}`);
 }
 
 export { printOwing };
