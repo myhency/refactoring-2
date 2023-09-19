25e2567008787913c38641ac253bf3b74fb86b43
myhency <hency.yeo@gmail.com>
2023-09-19 13:27:03 +0900

    feat: beforeEach 추가

--- test/produce-plan.spec.js
+++ test/produce-plan.spec.js
@@ -4,16 +4,17 @@ import { expect } from "chai";
 import { Province, sampleProvinceData } from "../src/chapter-4/index.js";
 
 describe("province", () => {
+    let asia;
+    beforeEach(() => {
+        asia = new Province(sampleProvinceData());
+    });
     it("shortfall", () => {
-        const asia = new Province(sampleProvinceData());
         assert.equal(asia.shortfall, 5);
     });
     it("shortfall", () => {
-        const asia = new Province(sampleProvinceData());
         expect(asia.shortfall).equal(5);
     });
     it("profit", () => {
-        const asia = new Province(sampleProvinceData());
         expect(asia.profit).equal(230);
     });
 });
