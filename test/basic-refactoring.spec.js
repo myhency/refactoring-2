2fc9d0179a337b786817d2ece79b376e42411b8a
myhency <hency.yeo@gmail.com>
2023-09-19 14:52:43 +0900

    feat: test code 추가

--- test/basic-refactoring.spec.js
+++ test/basic-refactoring.spec.js
@@ -1,7 +1,7 @@
 import { expect } from "chai";
 import sinon from "sinon";
 import { printOwing } from "../src/chapter-6/index.js";
-import { invoices } from "../src/data/index.js";
+import { owingInvoice as invoices } from "../src/data/index.js";
 
 describe("basic refactor", () => {
     let consoleLogSpy;
@@ -13,11 +13,21 @@ describe("basic refactor", () => {
         consoleLogSpy.restore();
     });
 
-    it("printOwing", () => {
+    it.skip("printOwing", () => {
         printOwing(invoices);
 
         expect(consoleLogSpy.calledWith("***********************")).to.be.true;
         expect(consoleLogSpy.calledWith("고객명: BigCo")).to.be.true;
         expect(consoleLogSpy.calledWith("채무액: 10")).to.be.true;
+    });
+
+    it("printOwing", () => {
+        printOwing(invoices);
+        expect(consoleLogSpy.calledWith("***********************")).to.be.true;
+        expect(consoleLogSpy.calledWith("**** 고객 채무 ****")).to.be.true;
+        expect(consoleLogSpy.calledWith("***********************")).to.be.true;
+        expect(consoleLogSpy.calledWith("고객명: BigCo")).to.be.true;
+        expect(consoleLogSpy.calledWith("채무액: 130")).to.be.true;
+        expect(consoleLogSpy.calledWith("마감일: 10/19/2023")).to.be.true;
     });
 });
