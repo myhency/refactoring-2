67fe5717396b1db6adcaab40176c9f293a81a1ff
myhency <hency.yeo@gmail.com>
2023-09-20 16:53:04 +0900

    refactor: 리팩터링 이후 테스트 변경 부분 추가

--- test/basic-refactoring-introduce-parameter-object.spec.js
+++ test/basic-refactoring-introduce-parameter-object.spec.js
@@ -1,6 +1,8 @@
 import {
     station,
     readingsOutsideRange,
+    operationPlan,
+    NumberRange,
 } from "../src/chapter-6/introduce-parameter-object.js";
 import { expect } from "chai";
 
@@ -10,7 +12,11 @@ describe("basic refactor", () => {
             { temp: 47, time: "2016-11-10 09:10" },
             { temp: 58, time: "2016-11-10 09:30" },
         ];
-        const result = readingsOutsideRange(station, 50, 55);
+        const range = new NumberRange(
+            operationPlan.temperatureFloor,
+            operationPlan.temperatureCeiling
+        );
+        const result = readingsOutsideRange(station, range);
         expect(result).to.deep.equal(expectedOutput);
     });
 });
