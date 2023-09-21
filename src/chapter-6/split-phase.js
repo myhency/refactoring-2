c821de5aa33124bf8a069a886afc67283380005e
myhency <hency.yeo@gmail.com>
2023-09-21 13:40:44 +0900

    refactor: 단계 쪼개기 리팩터링 이후 코드 추가

--- src/chapter-6/split-phase.js
+++ src/chapter-6/split-phase.js
@@ -10,19 +10,28 @@ const shippingMethod = {
     feePerCase: 20,
 };
 
-function priceOrder(product, quantity, shippingMethod) {
+function applyShipping(priceData, shippingMethod) {
+    const shippingPerCase =
+        priceData.basePrice > shippingMethod.discountThreshold
+            ? shippingMethod.discountedFee
+            : shippingMethod.feePerCase;
+    const shippingCost = priceData.quantity * shippingPerCase;
+    const price = priceData.basePrice - priceData.discount + shippingCost;
+    return price;
+}
+
+function calculatePricingData(product, quantity) {
     const basePrice = product.basePrice * quantity;
     const discount =
         Math.max(quantity - product.discountThreshold, 0) *
         product.basePrice *
         product.discountRate;
-    const shippingPerCase =
-        basePrice > shippingMethod.discountThreshold
-            ? shippingMethod.discountedFee
-            : shippingMethod.feePerCase;
-    const shippingCost = quantity * shippingPerCase;
-    const price = basePrice - discount + shippingCost;
-    return price;
+    return { basePrice, quantity, discount };
+}
+
+function priceOrder(product, quantity, shippingMethod) {
+    const priceData = calculatePricingData(product, quantity);
+    return applyShipping(priceData, shippingMethod);
 }
 
 try {
